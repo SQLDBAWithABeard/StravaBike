60912928c6bbc58051d94321051aa627b1760435
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-12-21 08:51:11 +0000

    one more time

--- visualiseData.py
+++ visualiseData.py
@@ -112,7 +112,7 @@ def produceActivtyRideHistogram():
     matplotlib.pyplot.xticks(fontsize=14,rotation=90)
     matplotlib.pyplot.yticks(fontsize=14)
     figure = matplotlib.pyplot.gcf()
-    figure.set_size_inches(8, 6)
+    figure.set_size_inches(4, 3)
     matplotlib.pyplot.savefig('Number_of_Rides_per_Distance.png', dpi=100)
     matplotlib.pyplot.clf()
 
